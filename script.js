51d918c942b30664fab590bdcd54b5cc8d3a6039
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-03-21 20:59:06 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// ##### selecting elements 
+
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const diceElement = document.querySelector('.dice');
+
+// ##### starting conditions 
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden')
+
+
